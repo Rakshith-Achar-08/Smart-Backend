5e8263f3c4241ed8a2c6c8e7744266626bbeb04b
Rakshith-Achar-08 <rakshithachar08@gmail.com>
2025-10-28 01:02:59 +0530

    .

--- src/main/java/com/BusFlow/backend/controller/BusController.java
+++ src/main/java/com/BusFlow/backend/controller/BusController.java
@@ -41,7 +41,6 @@ public class BusController {
 
     @GetMapping("/track-bus")
     public ResponseEntity<List<BusDetails>> searchBusesByRouteIdPrefix(@RequestParam String prefix) {
-
         List<BusDetails> buses = service.findBusesByRouteIdPrefix(prefix);
 
         if (buses.isEmpty()) {
